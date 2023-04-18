2881504881ad824e64ad27445d69c9e47a4b00ef
Anass Rouimi <rouimi.anass@gmail.com>
2023-04-18 14:34:48 +0200

    Importer du JS perso

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
+import { sampleText } from "./sampleText";
 
 function App() {
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-6">
-          <textarea className="form-control" rows="35"></textarea>
+          <textarea
+            value={sampleText}
+            className="form-control"
+            rows="35"
+          ></textarea>
         </div>
         <div className="col-sm-6">
-          <h1>Résultat</h1>
+          <div>{sampleText}</div>
         </div>
       </div>
     </div>
