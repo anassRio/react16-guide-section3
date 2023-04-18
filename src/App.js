fc661e4a2c7cbcb2314731dd6cb0ad62ef854c91
Anass Rouimi <rouimi.anass@gmail.com>
2023-04-18 14:42:52 +0200

    Utiliser et Modifier UseState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import { sampleText } from "./sampleText";
 
 function App() {
+  const [text, setText] = useState(sampleText);
+
+  const handleTextAreaChange = (event) => {
+    const modifiedText = event.target.value;
+    setText(modifiedText);
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-6">
           <textarea
-            value={sampleText}
+            value={text}
             className="form-control"
             rows="35"
+            onChange={handleTextAreaChange}
           ></textarea>
         </div>
         <div className="col-sm-6">
-          <div>{sampleText}</div>
+          <div>{text}</div>
         </div>
       </div>
     </div>
